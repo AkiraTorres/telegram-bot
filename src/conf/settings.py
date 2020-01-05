5783f9589b191839f02d572650aa0920a360a23f
PegasusAkira <torres.thg@gmail.com>
2020-01-05 09:04:15 -0300

    Dogs added

--- src/conf/settings.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-HTTP_CATS_URL = os.getenv("BASE_HTT_URL")
--- src/core.py
+++ src/core.py
@@ -1,8 +1,9 @@
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
-from conf.settings import TELEGRAM_TOKEN, HTTP_CATS_URL
-
-
+#from conf.settings import TELEGRAM_TOKEN, HTTP_CATS_URL
+TELEGRAM_TOKEN = "917350378:AAFJiETmnzcBXKU0mnJ6BTMx9QvER5P3S7k"
+HTTP_CATS_URL = "https://http.cat/"
+HTTP_DOGS_URL = "https://httpstatusdogs.com/"
 def start(bot, update):
     response_message = "=^._.^="
     bot.send_message(
@@ -14,10 +15,14 @@ def start(bot, update):
 def http_cats(bot, update, args):
     bot.sendPhoto(
         chat_id=update.message.chat_id,
-        #photo=HTTP_CATS_URL + args[0]
-        photo="https://http.cat/" +args[0]
+        photo=HTTP_CATS_URL + args[0]
     )
 
+def http_dogs(bot, update, args):
+    bot.sendPhoto(
+        chat_id=update.message.chat_id,
+        photo=HTTP_DOGS_URL + args[0]
+    )
 
 def unknown(bot, update):
     response_message = "Meow? =^._.^="
@@ -25,6 +30,16 @@ def unknown(bot, update):
         chat_id=update.message.chat_id,
         text=response_message
     )
+
+def help(bot, update):
+    responde_message = "Type '/help' to show this message.\
+                        \nType '/http [http-code-error]' to receive the corresponding cat image.\
+                        \nType '/dogs [http-code-error]' to receive the corresponding dog image."
+    bot.send_message(
+        chat_id=update.message.chat_id,
+        text=responde_message
+    )
+
 
 
 def main():
@@ -37,6 +52,12 @@ def main():
     )
     dispatcher.add_handler(
         CommandHandler('http', http_cats, pass_args=True)
+    )
+    dispatcher.add_handler(
+        CommandHandler('dogs', http_dogs, pass_args=True)
+    )
+    dispatcher.add_handler(
+        CommandHandler('help', help)
     )
     dispatcher.add_handler(
         MessageHandler(Filters.command, unknown)
